7ec3d7ef0d1525d699bd2e0efd2d292a6e26515e
AnnaBarczyk <56827312+AnnaBarczyk@users.noreply.github.com>
2020-10-05 11:22:39 +0200

    Add project files.

---
+++ Coins/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Vending_Machine_Kata.Coins.CoinsEnum;
+
+namespace Vending_Machine_Kata.Coins
+{
+    class Coin
+    {
+        public CoinsNames _coinName { get; set; }
+
+        public Coin(CoinsNames coinName)
+        {
+            _coinName = coinName;
+        }
+    }
+}
---
+++ Coins/CoinsEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vending_Machine_Kata.Coins
+{
+    public class CoinsEnum
+    {
+        public enum CoinsNames
+        {
+            Dime,
+            Nickel,
+            Pennie,
+            Quarter
+        }
+    }
+}
